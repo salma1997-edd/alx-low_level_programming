fcf72cf7a5c2711f324ef458138f220aa14c73f4
salma <eddaoudi@example.com>
2023-03-16 15:20:30 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - A programm that print the size
+* Return: 0 (Seccess)
+*/
+int main(void)
+{
+printf("size of char is= %i bytes\n", sizeof(char));
+printf("size of int  is= %i bytes\n", sizeof(int));
+printf("size of long int is= %i bytes\n", sizeof(long int));
+printf("size of long long int is= %i bytes\n", sizeof(long long int));
+printf("size of float is= %i bytes\n", sizeof(float));
+return (0);
+}

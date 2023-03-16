cf5bd39918a022f263972b4d44d10988d532f05e
salma <eddaoudi@example.com>
2023-03-16 13:59:46 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+*main -main entry point
+*return : 0 means succes
+*/
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-return(0);
-	}
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

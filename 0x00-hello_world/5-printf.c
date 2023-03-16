7a843a57e6e37fee18c3de31dc64b47000a0c6cf
salma <eddaoudi@example.com>
2023-03-16 14:46:52 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#includ < stdio.h >
+/**
+* main - A programm that print a message using printf
+* Return: 0 (Seccess)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,/n");
+return (0);
+}
